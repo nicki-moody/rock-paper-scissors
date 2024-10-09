b3d5238a08e6c1e264aba4a6b7832668e57074eb
Nicki Moody <nicki@re-echo.co.uk>
2024-10-09 21:22:55 +0100

    Refactored choice cards out into own component

--- app/routes/choices.$choiceId.tsx
+++ app/routes/choices.$choiceId.tsx
@@ -3,22 +3,9 @@ import { useState, useEffect } from "react";
 
 import Header from "~/components/Header";
 import Footer from "~/components/Footer";
+import ChoiceCard from "~/components/ChoiceCard";
 import { choices } from "~/utils/choices";
 import { determineWinner } from "~/utils/game";
-
-function ChoiceCard({choice}) {
-    const chosenChoice = choices.find((ch) => ch.choice === choice)
-    if (chosenChoice === undefined)
-        return ( <div>You did not select a valid choice</div>
-    )
-    return ( 
-        <div key={chosenChoice.choice} className={`${chosenChoice.outerCircle} flex rounded-full w-64 h-64 items-center justify-center`}>
-            <div className="flex bg-white rounded-full shadow-inner-strong w-48 h-48 items-center justify-center">
-            <img src={chosenChoice.icon} alt={chosenChoice.text} className="w-24"/>
-            </div>
-        </div>
-    ) 
-}
 
 export default function Choice() {
     const {choiceId} = useParams();
