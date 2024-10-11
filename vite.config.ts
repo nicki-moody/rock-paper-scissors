be4878a90cfe6b27e20eb8a5c28ff9f15063fd42
Nicki Moody <nicki@re-echo.co.uk>
2024-10-11 12:26:17 +0100

    Fixing vite:build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "../"),
+    },
+  },
   plugins: [
     remix({
       future: {
